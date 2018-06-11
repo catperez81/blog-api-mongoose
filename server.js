34d191c66ee67d3920d05b9580ded0e02eb41d90
catperez81 <catperez81@gmail.com>
2018-06-11 09:51:40 -0700

    faker install

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const faker = require('faker');
 
 mongoose.Promise = global.Promise;
 
