65af13c733b8179ecd2c8cee980c4f7f50349344
catperez81 <catperez81@gmail.com>
2018-06-11 09:48:39 -0700

    added faker to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const faker = require('faker');
 
 mongoose.Promise = global.Promise;
 
